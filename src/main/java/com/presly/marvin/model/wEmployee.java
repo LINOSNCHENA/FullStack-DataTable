affe0205191254cbdc1a0f7e656cd226729fa492
LINOS NCHENA <linosnchena@gmail.com>
2019-10-12 16:50:38 +0200

    selection options

--- src/main/java/com/presly/marvin/model/wEmployee.java
+++ src/main/java/com/presly/marvin/model/wEmployee.java
@@ -1,4 +1,5 @@
 package com.presly.marvin.model;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -6,11 +7,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-//import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "bank1")
 public class wEmployee {
+	
 	@Id
 	@Column(name = "id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,34 +28,33 @@ public class wEmployee {
 	@Column(name = "status")
 	private String status;
 
-	//@CreationTimestamp
    	@Column(name = "createdt",nullable = false,  columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-	private String createdt;
+	private Date createdt;
 
 public wEmployee(String name, String post, String dept,
-		int salary, String status, String createdt) {
+		int salary, String status, Date createdt) {
 		this.name = name;			this.post = post;		
 		this.dept = dept;			this.salary = salary;	
 		this.status = status;		this.createdt = createdt;	 }
 
-	public wEmployee()     			    {}
+public wEmployee()     			    {}
 	
-	public int getId()		 		    {	return id;			}
-	public void setId(int id) 		    {	this.id = id;		}
-	public String getName()  		    {	return name;		}
-	public void setName(String name)  {   this.name = name;	}
+public int getId()		 		    {	return id;			}
+public void setId(int id) 		    {	this.id = id;		}
+public String getName()  		    {	return name;		}
+public void setName(String name)  {   this.name = name;	}
 
-	public String getDept()  		  {	return dept;			}
-	public void setDept(String dept)  {	this.dept = dept;		}
-	public String getPost()  		  {	return post;			}
-	public void setPost(String post)  {	this.post = post;		}
+public String getDept()  		  {	return dept;			}
+public void setDept(String dept)  {	this.dept = dept;		}
+public String getPost()  		  {	return post;			}
+public void setPost(String post)  {	this.post = post;		}
 
-	public int getSalary()          		 {	return salary;		 	 }
-	public void setSalary(int salary)		 {	this.salary = salary;	 }
-	public String getStatus()  		 	     {	return status;		 	 }
-	public void setStatus(String status)     {	this.status = status; 	 }
+public int getSalary()          		 {	return salary;		 	 }
+public void setSalary(int salary)		 {	this.salary = salary;	 }
+public String getStatus()  		 	     {	return status;		 	 }
+public void setStatus(String status)     {	this.status = status; 	 }
 
-	public String getCreatedt()  		 	 {	return createdt;		  }
-	public void setCreatedt(String createdt) {	this.createdt = createdt; }
+public Date getCreatedt()  		 	   {	return createdt;		  }
+public void setCreatedt(Date createdt) {	this.createdt = createdt; }
 
 }
--- src/main/resources/SchemaMySQL.sql
+++ src/main/resources/SchemaMySQL.sql
@@ -7,13 +7,13 @@ id INT auto_increment PRIMARY KEY,
 name VARCHAR (50) NOT NULL,  
 dept VARCHAR (50) NOT NULL,
 post VARCHAR (50) NOT NULL, 
-salary int NOT NULL default 8080,
-status VARCHAR (50) NOT NULL DEFAULT "Temporal",
+salary int default 8080 not null,
+status VARCHAR (50) DEFAULT "Temporal" not null,
 createdt DATETIME default now());
 
 INSERT INTO bank1 ( name, dept, post, salary )
  VALUES
  ("KENNETH KAUNDA", "Enginering","Director", 21200 ),   ("Lorena Marvin", "Admin","Administrator", 18500 ),
- ("Nikolas Marvin", "Engineering","Manager", 18500 ),( "JURIOUS NYERERE", "PRESIDENT","TANZANIA", 32500 );
+ ("Nikolas Marvin", "Engineering","Manager", 18500 ),  ("JURIOUS NYERERE", "PRESIDENT","TANZANIA", 32500 );
 
 select* from bank1
