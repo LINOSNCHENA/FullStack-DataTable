0262a88da0483177ff9738d41816bf006c0820fb
Linos Nchena <linosnchena@gmail.com>
2020-08-20 15:11:42 +0200

    POSTGRESS DATABASE

--- src/main/resources/SchemaPostGre.sql
+++ src/main/resources/SchemaPostGre.sql
@@ -1,17 +1,24 @@
-DROP TABLE IF EXISTS bank1 ; 
+DROP TABLE IF EXISTS bank1
+;
 
-CREATE TABLE bank1 (
-  id serial PRIMARY KEY,   
+CREATE TABLE bank1
+(
+  id serial PRIMARY KEY,
   name VARCHAR(90) NOT NULL,
-  post VARCHAR(90), 
+  post VARCHAR(90),
   dept VARCHAR(90) default 'HUMAN-RESOURCE',
-  salary int default 62005,  
-  status VARCHAR(90) default 'PART-TIME', 
-  createdt TIMESTAMP DEFAULT CURRENT_TIMESTAMP );
+  salary int default 62005,
+  status VARCHAR(90) default 'PART-TIME',
+  createdt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-INSERT INTO bank1 ( name, dept, post, salary )
- VALUES
- ('KENNETH KAUNDA', 'Enginering','Director', 21200 ),  ('Lorena Marvin', 'POSTGRESS','Administrator', 18500 ),
- ('Nikolas Marvin', 'Engineering','Manager', 18500 ),  ( 'JURIOUS NYERERE', 'PRESIDENT','TANZANIA', 32500 );
- 
-select * from bank1
+INSERT INTO bank1
+  ( name, dept, post, salary )
+VALUES
+        ("Goliath Nelson", "Enginering", "Developer", 21200 ),
+        ("Lorena Marvin", "Accounts", "Administrator", 18500 ),
+        ("Nicholas Presly", "Engineering", "Manager", 18500 ),
+        ("Wendy Leon", "PostgreSQL Data", "Tester Developer", 32500 );
+
+select *
+from bank1
